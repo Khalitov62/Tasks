e0866a1118cdc719ceff4dfbf96de869d5638a87
Khalitov62 <tu7m5rk73code.edu.az>
2022-03-28 13:17:01 +0400

    added

---
+++ ConsoleApp20/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+
+namespace ConsoleApp20
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(CountWords());
+            Console.WriteLine(isUpper());
+        }
+
+        public static int CountWords()
+        {
+            string sentence = "Salam Aleykum";
+            var wordCount = 0;
+            for (var i = 0; i < sentence.Length; i++)
+            {
+                if (sentence[i] == ' ' || i == sentence.Length -1)
+                {
+                    wordCount++;
+                }
+            }
+            return wordCount;
+        }
+        public static bool isUpper()
+        {
+            string sentence = "Task qisadi";
+            int count = 0;
+            for (int i = 0; i < sentence.Length; i++)
+            {
+                if (char.IsUpper(sentence[i]) && i == 0)
+                {
+                    count++;
+
+                }
+                else if (char.IsWhiteSpace(sentence[i]))
+                {
+                    count++;
+                    if(char.IsUpper(sentence[i + 1]))
+                    {
+                        count++;
+                    }
+                }
+            }
+            return count ==3 ? true : false;
+        }
+    }
+}
---
+++ ConsoleApp21/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace ConsoleApp21
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] array = { 1, 2, 3, 4, 5, 6 };
+            Add(ref array);
+
+        }
+        public static void Add(ref int [] array)
+        {
+            Array.Resize(ref array, array.Length + 1);
+            array[array.Length - 1] = 1;
+            foreach (var number in array)
+            {
+                Console.WriteLine(number);
+            }
+
+        }
+    }
+}
